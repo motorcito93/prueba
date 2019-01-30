5a863c5cebe6cfd2d2d39ff64c25b581d00bf93c
motorcito <motorcito@motorcito>
2019-01-30 13:52:44 +0100

    segundo commit

--- src/pruebagh/Pruebagh.java
+++ src/pruebagh/Pruebagh.java
@@ -16,6 +16,7 @@ public class Pruebagh {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //* este es un comentario en el codigo
     }
     
 }
